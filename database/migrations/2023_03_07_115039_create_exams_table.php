d332380becc47fed829b1834008005b0ba7a7626
Edrees1582 <31400075+Edrees1582@users.noreply.github.com>
2023-03-07 14:14:15 +0200

    Initial commit

---
+++ database/migrations/2023_03_07_115039_create_exams_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('numberOfQuestions');
+            
+            $table->string('question_1');
+            $table->string('question_2');
+            $table->string('question_3');
+            $table->string('question_4');
+            $table->string('question_5');
+
+            $table->string('question_1_ans');
+            $table->string('question_2_ans');
+            $table->string('question_3_ans');
+            $table->string('question_4_ans');
+            $table->string('question_5_ans');
+
+            $table->integer('grade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exams');
+    }
+};
